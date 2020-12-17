d324ec2824b0f2625ac0eb1529f954a76333bcad
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-17 08:42:21 +0000

    reshape users#create controller to accomodate jwt encoding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized
+
   def create
+    binding.pry
   	user = User.find_by(username: user_params[:username])
   	if user && !user.authenticate(user_params[:password])
   		user = nil
   	elsif !user && !user_params[:password].empty?
 	  	user = User.create(params.permit(:username, :password))
 	  end
-	  render json: UserSerializer.new(user).to_serialized_json
+    @token = encode_token(user_id: user.id) if user       
+    render json: { user: user, jwt: @token }, status: :created
   end
 
   private
