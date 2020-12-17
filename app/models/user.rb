0b063a3f30f81605e56243e70c06e017dbd94f7b
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-17 15:57:24 +0000

    add validation for lenghts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
 	# has_many :stakes
 	has_secure_password
+
+	validates :username, presence: { message: "%{attribute} must be given" }, length: {maximum: 10}
+  validates :password, length: {maximum: 10}
 end
