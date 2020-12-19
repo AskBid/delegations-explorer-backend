3175de787c539f1256467ade42a7daf47a3b9d2f
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-19 06:27:27 +0000

    users#create now behaves better taking into consideration all possible mistakes/errors the user can input and it always renders correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,25 @@ class UsersController < ApplicationController
   	if @user && !@user.authenticate(user_params[:password])
   		@user.errors.messages[:password] = "Wrong password."
       @user.errors.messages[:username] = "Username exist. (#{@user.username})"
+      render json: {errors: @user.errors.messages}
   	elsif !@user
-	  	@user = User.create(params.permit(:username, :password))
-	  end
-    @token = encode_token(user_id: @user.id) if @user
-    render json: UserSerializer.new(@user).to_serialized_json(token: @token, errors: @user.errors.messages)
-  end
-
-  def show
-    @user = current_user
-    render json: UserSerializer.new(@user).to_serialized_json
+	  	@user = User.new(user_params)
+       if !@user.save
+          render json: {errors: @user.errors.messages}
+       else
+          render_user
+       end
+    else
+      render_user
+    end
   end
 
   private
+
+  def render_user
+    @token = encode_token(user_id: @user.id)
+    render json: UserSerializer.new(@user).to_serialized_json(token: @token, errors: @user.errors.messages)
+  end
 
   def user_params
   	params.permit(:username, :password)
