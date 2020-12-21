a0a6f16571be712d63bf0607ccbeee76c6edae4e
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-21 23:20:50 +0000

    first steps for stakes#index

--- app/controllers/stakes_controller.rb
+++ app/controllers/stakes_controller.rb
@@ -1,14 +1,24 @@
-class UsersController < ApplicationController
+class StakesController < ApplicationController
   skip_before_action :authorized
 
   def index
+    binding.pry
+    @user = current_user
+    if @user
+      @stakes = @user.stakes
+      render json: @stakes 
+    end
+    render json: {message: "There isn't logged in user"}
   end
 
   def create
+    binding.pry 
+    render json: {success: 'data was sent from create'}
   end
 
   private
 
   def stake_params
+    params.permit(:user_id)
   end
 end
