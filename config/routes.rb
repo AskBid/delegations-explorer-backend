bb6ab10660c3f1be472bd2a856f63e5bad6f308b
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-22 20:43:08 +0000

    post followed_pool route, as we are going to add the functionality in the front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:create] do
   	resources :stakes, only: [:index, :create]
+  	resources :pools, only: [:create]
   end
   get 'login', to: 'sessions#restore'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
