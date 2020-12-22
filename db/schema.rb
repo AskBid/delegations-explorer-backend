317baf1f44852612ea1115d29265e0fa522965cf
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-22 20:41:27 +0000

    add model to allow User to follow Pools in order to compare real delegations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_070749) do
+ActiveRecord::Schema.define(version: 2020_12_22_195624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2020_12_19_070749) do
     t.string "rewards"
     t.integer "pool_id"
     t.integer "stake_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "pool_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "pool_owners", force: :cascade do |t|
