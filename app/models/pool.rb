b32f3680e9d1f5513a3917f89792ac7ee492a0e7
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-22 20:42:27 +0000

    extending associations of user and pool to follow pools and yet give the possibility to see actual delegasted pools

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -2,6 +2,8 @@ class Pool < ApplicationRecord
 	has_many :active_stakes
 	has_many :pool_reward_addresses
 	has_many :pool_owners
+	has_many :follows
+	has_many :users, through: :follow
 
 	def self.clean_all_tickers
 		self.all.each do |pool|
