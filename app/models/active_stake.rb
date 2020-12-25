dc796432f2b0bef9915472d47b8b0f8adc376129
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-25 09:34:27 +0000

    add methods to calculate the mode average of the ratio between delegation/rewards for an epoch

--- app/models/active_stake.rb
+++ app/models/active_stake.rb
@@ -5,4 +5,5 @@ class ActiveStake < ApplicationRecord
 	validates_uniqueness_of :stake_id, scope: [:epochno], message: "%{value} already has this epochNo"
 
 	scope :by_epochno, -> (epochno) {where('epochno = ?', epochno)}
+	scope :by_pool, -> (pool_id) {where('pool_id = ?', pool_id)}
 end
--- app/models/pool.rb
+++ app/models/pool.rb
@@ -5,11 +5,31 @@ class Pool < ApplicationRecord
 	has_many :follows
 	has_many :users, through: :follow
 
-	def self.clean_all_tickers
+	def reward_ratio(epoch = ActiveStake.maximum('epochno')) {
+		as = self.active_stakes.by_epoch(epoch)
+		values = as.map do |as|
+			if as.rewards.to_i != 0 && as.rewards
+				as.amount.to_i / as.rewards.to_i
+			end
+		end
+		modes(values)
+	}
+
+	def self.empty_all_tickers
 		self.all.each do |pool|
 			pool.ticker = nil
 			pool.save
 		end
 		return true
+	end
+
+	def modes(array, find_all=true)
+	  histogram = array.inject(Hash.new(0)) { |h, n| h[n] += 1; h }
+	  modes = nil
+	  histogram.each_pair do |item, times|
+	    modes << item if modes && times == modes[0] and find_all
+	    modes = [times, item] if (!modes && times>1) or (modes && times>modes[0])
+	  end
+	  return modes ? modes[1...modes.size] : modes
 	end
 end
