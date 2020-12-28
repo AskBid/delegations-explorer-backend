126e0803e3fa8e045fb9e11b504907f677691c64
Sergio Marelli <sergio.mrll@gmail.com>
2020-12-28 21:49:25 +0000

    correction of application name in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 
-set :application, "delegation-explorer-backend"
+set :application, "delegation-explorer"
 set :repo_url, "git@github.com:AskBid/delegations-explorer-backend.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rvm_ruby_version, '2.6.3'
